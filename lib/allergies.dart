1125d0d8e5b87ef97e001f774d7a4ad53f9849f7
aishwarya-panampilly <aishwarya.panampilly@gmail.com>
2023-05-12 17:05:46 +0530

    Added some allergies

--- lib/allergies.dart
+++ lib/allergies.dart
@@ -1,6 +1,13 @@
-enum CaloriesAllergy{
-  lactose(preferenceKey:'allergy_lactose', text: 'Lactose'),
-  peanut(preferenceKey:'allergy_peanut', text:'Peanut');
+enum CaloriesAllergy {
+  milk(preferenceKey: 'allergy_lactose', text: 'Milk'),
+  peanut(preferenceKey: 'allergy_peanut', text: 'Peanut'),
+  egg(preferenceKey: 'allergy_egg', text: 'Egg'),
+  fish(preferenceKey: 'allergy_fish', text: 'Fish'),
+  soy(preferenceKey: 'allergy_soy', text: 'Soy'),
+  celery(preferenceKey: 'allergy_celery', text: 'Celery'),
+  gluten(preferenceKey: 'allergy_gluten', text: 'Gluten'),
+  crustaceans(preferenceKey: 'allergy_crustaceans', text: 'Crustaceans'),
+  treenuts(preferenceKey: 'allergy_treenuts', text: 'Treenuts');
 
   const CaloriesAllergy({
     required this.preferenceKey,
