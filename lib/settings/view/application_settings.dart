6cbaee94ce03cf5ec78a69ec5a5f162809f3476f
aishwarya-panampilly <aishwarya.panampilly@gmail.com>
2023-06-07 13:34:16 +0530

    Minor settings mess up cleaned

--- lib/settings/view/application_settings.dart
+++ lib/settings/view/application_settings.dart
@@ -16,7 +16,6 @@ class CaloriesApplicationSettingsPage extends ConsumerStatefulWidget {
 
 class _CaloriesApplicationSettingsPageState
     extends ConsumerState<CaloriesApplicationSettingsPage> {
-
   @override
   Widget build(BuildContext context) {
     var asyncSettings = ref.watch(settingsProvider);
@@ -25,58 +24,73 @@ class _CaloriesApplicationSettingsPageState
         title: Text(widget.title),
       ),
       body: asyncSettings.when(
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error: (err, stackTrace) {
-          developer.log("$err", name: 'calories_app.settings.application_settings');
-          return const Text("Error loading settings!");
-        },
-        data: (settings) {
-          return Column(
-            children: <Widget>[
-              ListTile(
-                title: const Text("Manual Input Mode"),
-                trailing: Switch(
-                  value: settings.manualInputMode,
-                  onChanged: (value) {
-                    ref.read(settingsNotifier.notifier).set(manualInputMode: value);
-                  },
+          loading: () => const Center(child: CircularProgressIndicator()),
+          error: (err, stackTrace) {
+            developer.log("$err",
+                name: 'calories_app.settings.application_settings');
+            return const Text("Error loading settings!");
+          },
+          data: (settings) {
+            return Column(
+              children: <Widget>[
+                ListTile(
+                  title: const Text("Manual Input Mode"),
+                  trailing: Switch(
+                    value: settings.manualInputMode,
+                    onChanged: (value) {
+                      ref
+                          .read(settingsNotifier.notifier)
+                          .set(manualInputMode: value);
+                    },
+                  ),
                 ),
-              ),
-              ListTile(
-                title: const Text("Theme"),
-                trailing: DropdownMenu<String>(
-                  dropdownMenuEntries: ['System Default','Light', 'Dark'].map((e) => DropdownMenuEntry<String>(value: e, label: e)).toList(),
-                  width: null,
-                  initialSelection: settings.theme=='light'?'Light':(settings.theme == 'dark'?'Dark':'System Default'),
-                  onSelected: (s) {
-                    if(s == 'Light')
-                    {
-                      ref.read(settingsNotifier.notifier).set(theme: ThemeMode.light);
-                      ref.read(themeChangeNotifierProvider.notifier).changeTheme(ThemeMode.light);
-                    }
-                    else if(s == 'Dark')
-                    {
-                      ref.read(settingsNotifier.notifier).set(theme: ThemeMode.dark);
-                      ref.read(themeChangeNotifierProvider.notifier).changeTheme(ThemeMode.dark);
-                    }
-                    else
-                    {
-                      ref.read(settingsNotifier.notifier).set(theme: ThemeMode.system);
-                      ref.read(themeChangeNotifierProvider.notifier).changeTheme(ThemeMode.system);
-                    }
-                  },
-                )
-              ),
-              const ListTile(
-                title: Text("Clear Data"),
-              ),
-              const ListTile(
-                title: Text("Developer Info"),
-              ),
-            ],
-          );
-        }
-      ),
+                ListTile(
+                    title: const Text("Theme"),
+                    trailing: DropdownMenu<String>(
+                      dropdownMenuEntries: ['System Default', 'Light', 'Dark']
+                          .map((e) =>
+                              DropdownMenuEntry<String>(value: e, label: e))
+                          .toList(),
+                      width: null,
+                      initialSelection: settings.theme == ThemeMode.light
+                          ? 'Light'
+                          : (settings.theme == ThemeMode.dark
+                              ? 'Dark'
+                              : 'System Default'),
+                      onSelected: (s) {
+                        if (s == 'Light') {
+                          ref
+                              .read(settingsNotifier.notifier)
+                              .set(theme: ThemeMode.light);
+                          ref
+                              .read(themeChangeNotifierProvider.notifier)
+                              .changeTheme(ThemeMode.light);
+                        } else if (s == 'Dark') {
+                          ref
+                              .read(settingsNotifier.notifier)
+                              .set(theme: ThemeMode.dark);
+                          ref
+                              .read(themeChangeNotifierProvider.notifier)
+                              .changeTheme(ThemeMode.dark);
+                        } else {
+                          ref
+                              .read(settingsNotifier.notifier)
+                              .set(theme: ThemeMode.system);
+                          ref
+                              .read(themeChangeNotifierProvider.notifier)
+                              .changeTheme(ThemeMode.system);
+                        }
+                      },
+                    )),
+                const ListTile(
+                  title: Text("Clear Data"),
+                ),
+                const ListTile(
+                  title: Text("Developer Info"),
+                ),
+              ],
+            );
+          }),
     );
   }
 }
